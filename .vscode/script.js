a3f63f226a31d83421da5709f8f618c9a09b1ec2
samanthadotdesign <samleeml@gmail.com>
2021-04-19 22:52:43 +0800

    basic layout to arrange cards

---
+++ .vscode/script.js
@@ -0,0 +1,12 @@
+// Game play
+
+// Selectors
+const entryDiv = document.querySelector(".entry-div");
+const gameDiv = document.querySelector(".game-div");
+
+const initGame = () => {
+  // Hide the opening window
+  entryDiv.style.display = "none";
+};
+
+initGame();

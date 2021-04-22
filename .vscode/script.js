41c22261eef012f0d1f2cfec108bae3d0a0bfdcc
samanthadotdesign <samleeml@gmail.com>
2021-04-22 09:51:18 +0800

    tally cards function to calculate hand score

--- .vscode/script.js
+++ .vscode/script.js
@@ -5,57 +5,15 @@ const entryDiv = document.querySelector(".entry-div");
 const gameDiv = document.querySelector(".game-div");
 const cardsDiv = document.querySelector(".cards-div");
 const scoreTag = document.querySelector(".player-score");
+const messageDiv = document.querySelector(".message-div");
+
+// Message
+const messageTag = document.querySelector(".message");
 
 // Buttons
 const startBtn = document.querySelector(".start-btn");
 const dealBtn = document.querySelector(".deal-btn");
 const playBtn = document.querySelector(".play-btn");
-
-/* ------- HAND RANKING FUNCTIONS ------ */
-
-// Detects whether poker hand contains a hand rank
-// Returns false otherwise
-
-// Need to add a joker in the hand
-// Rank 1: Five of a kind
-// Rank 2: Straight flush
-
-// Takes in an array and returns a number score
-const calcHandScore = (arr) => {
-  const score = 0;
-  for (i = 0; i < arr.length; i += 1) {
-    score += arr[i].rank;
-  }
-  return score;
-};
-
-// Rank 3: Four of a kind
-const is4OfAKind = (arr) => {};
-
-// Rank 4: Full house
-const isFullHouse = (arr) => {};
-
-// Rank 5: Flush
-const isFLush = (arr) => {};
-
-// Rank 6: Straight
-const isStraight = (arr) => {};
-
-// Rank 7: Three of a kind
-const is3OfAKind = (arr) => {};
-
-// Rank 8: Two pair
-const is2Pair = (arr) => {};
-
-// Rank 9: One pair
-const is1Pair = (arr) => {};
-
-// Rank 10: High card
-const isHighCard = (arr) => {};
-
-// Take an array of card objects and return the number of points that the user scored for the cards in their hand
-
-/* ------- WINNING LOGIC ------ */
 
 /* ------- HELPER FUNCTIONS FOR CARDS ------ */
 
@@ -101,6 +59,8 @@ const replaceCards = () => {
   }
 };
 
+// Additional: User cannot remove more than 3 cards
+// If user clicks on the 4th empty card, print "you can only remove 3 cards"
 // Removes card from display at that position and changes the hand array
 const removeCardDisplay = (index) => {
   console.log("clicked on element number ", index);
@@ -169,7 +129,7 @@ playBtn.addEventListener("click", () => {
 });
 
 // Takes in array and calculate score of the ranks
-const calcHand = (arr) => {
+const basicScore = (arr) => {
   let score = 0;
   for (i = 0; i < arr.length; i += 1) {
     score += arr[i].rank;
@@ -177,3 +137,120 @@ const calcHand = (arr) => {
   }
   return score;
 };
+
+// Score begins at 100
+const score = 100;
+
+// Takes in array hand of cards, prints in HTML, gives the multiplier score
+const calcHand = (arr) => {
+  const tallyObj = tallyCards(hand);
+  console.log(`tally object is `, tallyObj);
+
+  // [1] Straight flush
+  for (i = 0; i < arr.length; i += 1) {
+    if (hand[i].suit === hand[)
+  }
+  // Same suit & increment by one
+  // const containsAll = (arr1, arr2) => 
+  // arr2.every(arr2Item => arr1.includes(arr2Item))
+  // const sameMembers = (arr1, arr2) => 
+  // containsAll(arr1, arr2) && containsAll(arr2, arr1);
+  // sameMembers(arr1, arr2);
+
+  // Array of keys
+  // const cardNames = Object.keys(tallyObj);
+  // Test for straight
+
+  // Array of values
+  // const cardValues = Object.values(tallyObj);
+
+  for (const [cardType, cardCount] of Object.entries(tallyObj)) {
+    console.log(`there is ${cardCount} ${cardType}s in the hand`); // 2 As in the hand
+
+    // [1] Four of a kind
+    if (cardCount === 4) {
+      printMessage(`Four of a kind of ${cardType}`);
+    }
+
+    // [2] Three of a kind
+    if (cardcount === 3) {
+      printMessage(`Three of a kind of ${cardType}`);
+    }
+
+
+
+  }
+
+  // for (i = 0; i < values.length; i += 1) {
+  //   if (value[i] === 4) {
+
+  //     printMessage(`Four of a kind of ${}`);
+  //     score;
+  //   }
+  // }
+
+  // Array [key, property] of arrays [ [" ", num] , [" ", num] , [" ", num] , [" ", num] ]
+  // const cardEntries = Object.entries(tallyObj);
+
+  return score;
+};
+
+/* ------- HAND RANKING FUNCTIONS ------ */
+
+// Adds list-item about what is true inside the hand
+const printMessage = (message) => {
+  const listItem = document.createElement("li");
+  listItem.innerHTML = message;
+  messageTag.appendChild(listItem);
+};
+
+// Four of a kind
+const is4OfAKind = (values) => {};
+
+// Rank 4: Full house
+const isFullHouse = (arr) => {};
+
+// Rank 5: Flush
+const isFLush = (arr) => {};
+
+// Rank 6: Straight
+const isStraight = (arr) => {};
+
+// Rank 7: Three of a kind
+const is3OfAKind = (arr) => {};
+
+// Rank 8: Two pair
+const is2Pair = (arr) => {};
+
+// Rank 9: One pair
+const is1Pair = () => {};
+
+// Rank 10: High card
+const isHighCard = (arr) => {};
+
+/* --------------------------- */
+
+// Takes in an array of objects
+// Returns object to store how many times a card type occurs
+const tallyCards = (arr) => {
+  const cardTypes = {};
+
+  for (i = 0; i < hand.length; i += 1) {
+    // Get the name of the card
+    const cardType = hand[i].name;
+
+    // See if we have already recorded
+    // One card in the tally already
+    if (cardTypes[cardType] === undefined) {
+      // This card has never been seen before
+      // Start with tally of one
+      cardTypes[cardType] = 1;
+    } else {
+      // Increment the tally
+      cardTypes[cardType] += 1;
+    }
+  }
+  return cardTypes;
+};
+
+/** */

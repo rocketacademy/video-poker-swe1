9d67262339a97d55cae8dee7529299928031ef60
ZAF07 <zaffere07@gmail.com>
2021-05-03 16:10:36 +0800

    Edited path for href in index.html for game page

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 // const fs = require('fs');
 
+const { readFile } = require('fs');
 const http = require('http');
+const path = require('path');
 
 const PORT = process.argv[2];
+const file = './index.html';
 
 http
   .createServer((req, res) => {
-    res.end('Hello');
+    const fileName = req.url;
+    const extentionName = String(path.extname(fileName)).toLocaleLowerCase();
+    console.log('THIS IS REQ -> ', fileName);
+
+    readFile(file, (err, content) => {
+      if (!err) {
+        res.end(content);
+        return;
+      }
+      console.log(err);
+    });
   })
   .listen(PORT, console.log(`Server listening on port ${PORT}`));

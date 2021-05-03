0f2fa8aca66fc5de89e8b21b25d3e5d828fc2260
ZAF07 <zaffere07@gmail.com>
2021-05-03 15:56:53 +0800

    Added the res.end

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const PORT = process.argv[2];
 
 http
   .createServer((req, res) => {
-    res.send('Hello');
+    res.end('Hello');
   })
   .listen(PORT, console.log(`Server listening on port ${PORT}`));
